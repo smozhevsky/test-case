da840aa36d2723732fa8b5a7e2ed89128754da9d
smozhevsky <smozhevsky@gmail.com>
2021-03-18 08:44:51 +0300

    add some changes in case

--- js/main.js
+++ js/main.js
@@ -1,10 +1,38 @@
 const playButton = document.querySelector(".main-content__button");
 
-// let link = "https://www.youtube.com/watch?v=mhDJNfV7hjk";
+let link = "https://www.youtube.com/watch?v=mhDJNfV7hjk";
 
 // playButton.addEventListener("click", () => {
 //   window.open(link);
 // });
+
+// class ModalWindow {
+//   constructor(btn, url) {
+//     this.btn = btn;
+//     this.url = url;
+//   }
+
+// init() {
+//   // подписываемся на клик
+//     window.addEventListener("keydown", function (e) {
+//       //закрытие окна по escape
+//       if (e.keyCode == 27 && this.isOpened) {
+//           e.preventDefault();
+//           this.close();
+//           return;
+//       }
+//       // управление по tab
+//       if (e.keyCode == 9 && this.isOpened) {
+//         this.focusCatcher(e);
+//         return;
+//     }
+//   }
+// }
+
+//   destroy() {
+//     // отписываемся
+//   }
+// }
 
 class ModalWindow {
   constructor(btn, url) {
@@ -13,23 +41,17 @@ class ModalWindow {
   }
 
   init() {
-    // подписываемся на клик
-    //   window.addEventListener("keydown", function (e) {
-    //     //закрытие окна по escape
-    //     if (e.keyCode == 27 && this.isOpened) {
-    //         e.preventDefault();
-    //         this.close();
-    //         return;
-    //     }
-    //     // управление по tab
-    //     if (e.keyCode == 9 && this.isOpened) {
-    //       this.focusCatcher(e);
-    //       return;
-    //   }
-    // }
+    this.btn.addEventListener("click", () => {
+      window.open(link);
+    });
   }
 
   destroy() {
-    // отписываемся
+    this.btn.removeEventListener("click", () => {
+      window.open(link);
+    });
   }
 }
+
+let modality = new ModalWindow(playButton, link);
+modality.init();
